d6a1d4481610c7d922d3dfbf62693f44f81e130e
Jeff McAleer <jeff.mcaleer@avispl.com>
2023-05-18 09:16:56 -0400

    chore: Updated Command List Parser, parses info tool scripts as well

--- commands/create-command-list.js
+++ commands/create-command-list.js
@@ -6,6 +6,10 @@ let data = {
     actions: [],
 };
 let actions = [];
+let userCommands = [];
+
+let commandRegex = /(?<command>\S+)\s+(?<access>\S*)\s+(?<description>.*)/m;
+let userCommandRegex = /(?<command>\S+):\d+\s+(?<access>\S*)\s+(?<description>.*)/m;
 
 function readFiles(dirname, onFileContent, onError) {
     fs.readdirSync(dirname).forEach((filename) => {
@@ -15,32 +19,55 @@ function readFiles(dirname, onFileContent, onError) {
 }
 
 function readFileContent(content) {
-    let commandListRegex = /^(?<model>\S*)>help all\r?\n(?<commandList>(?:^(\S+)\s*(\S+)\s*(.+)\r?\n)+)/m;
-    let matches = content.match(commandListRegex);
-    if (matches && matches.groups) {
-        parseCommandList(matches.groups.model, matches.groups.commandList);
-    } else {
-        console.log('nope');
+    let modelRegex = /^(?<model>\S*)>/m;
+    let modelMatches = content.match(modelRegex);
+
+    if (modelMatches && modelMatches.groups) {
+        let model = modelMatches.groups.model;
+
+        userCommands = [];
+        let userCommandListRegex = /help user\r?\n(?<commandList>(?:^(\S+)\s*(\S+)\s*(.+)\r?\n)+)/m;
+        let userCommandMatches = content.match(userCommandListRegex);
+        if (userCommandMatches && userCommandMatches.groups) {
+            parseUserCommandList(userCommandMatches.groups.commandList);
+        }
+
+        let commandListRegex = /help all\r?\n(?<commandList>(?:^(\S+)\s*(\S+)\s*(.+)\r?\n)+)/m;
+        let commandListMatches = content.match(commandListRegex);
+        if (commandListMatches && commandListMatches.groups) {
+            parseCommandList(model, commandListMatches.groups.commandList);
+        }
     }
+}
+
+function parseUserCommandList(commandList) {
+    let commands = commandList.split('\n');
+
+    commands.forEach((command) => {
+        let matches = command.match(userCommandRegex);
+        if (matches) {
+            if (!userCommands.includes(matches.groups.command)) {
+                userCommands.push(matches.groups.command);
+            }
+        }
+    });
 }
 
 function parseCommandList(model, commandList) {
     let commands = commandList.split('\n');
-    let commandRegex = /(?<command>\S+)\s+(?<access>\S*)\s+(?<description>.*)/m;
+
     commands.forEach((command) => {
         let matches = command.match(commandRegex);
         if (matches) {
-            addAction(matches.groups.command, matches.groups.access, matches.groups.description, model);
+            // Check to make sure this isn't a custom user command for the specific program running on this machine.
+            if (!userCommands.includes(matches.groups.command)) {
+                addAction(matches.groups.command, matches.groups.access, matches.groups.description, model);
+            }
         }
     });
 }
 
 function addAction(command, access, description, model) {
-    // if (description.startsWith('(*)')) {
-    //     // These are custom user commands for specific projects
-    //     return;
-    // }
-
     let action = data.actions.find((action) => action.command == command);
     if (action) {
         if (!action.models.includes(model)) {
